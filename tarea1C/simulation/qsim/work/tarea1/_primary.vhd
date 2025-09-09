2c4bcd2d523c832272c0100dea20602d1faa16bb
estudiante <estudiante@labwin.local>
2025-09-09 19:01:04 -0300

    funcionando todo tarea1

---
+++ tarea1C/simulation/qsim/work/tarea1/_primary.vhd
@@ -0,0 +1,15 @@
+library verilog;
+use verilog.vl_types.all;
+entity tarea1 is
+    port(
+        LED9            : out    vl_logic;
+        SW              : in     vl_logic_vector(9 downto 0);
+        LED8            : out    vl_logic;
+        LED7            : out    vl_logic;
+        LED6            : out    vl_logic;
+        LED3            : out    vl_logic;
+        LED2            : out    vl_logic;
+        LED1            : out    vl_logic;
+        LED0            : out    vl_logic
+    );
+end tarea1;
---
+++ tarea1C/simulation/qsim/work/tarea1_vlg_vec_tst/_primary.vhd
@@ -0,0 +1,4 @@
+library verilog;
+use verilog.vl_types.all;
+entity tarea1_vlg_vec_tst is
+end tarea1_vlg_vec_tst;
